ef78bd91f01ca1f2950d6da08ff6d0aaedb554e5
DYRuiz <73978149+DYRuiz@users.noreply.github.com>
2021-02-20 21:09:23 -0800

    Update charts.js

--- charts.js
+++ charts.js
@@ -98,7 +98,6 @@ function buildCharts(sample) {
     // 9. Create the layout for the bar chart. 
     var barLayout = {
      title: "Top 10 Bacteria Cultures Found"
-     }
     };
 
     // 10. Use Plotly to plot the data with the layout. 
